a539b595ca8bfc969ed2604d7e60b03deae0b1f2
abdirahman <57835339+Butugey12@users.noreply.github.com>
2023-02-11 08:50:04 +0200

    moving files to folders

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/app.css"
+function App() {
+  return (
+    <div className="App">
+      <h1>Nigaaaaaaaa</h1>
+    </div>
+  );
+}
+
+export default App;
